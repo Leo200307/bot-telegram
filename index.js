4a04e86422b21c627326b03fdda0bf9d9a9ae060
soletoranul-droid <soletoranul@gmail.com>
2026-06-01 22:00:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ bot.on('callback_query', async (query) => {
             await bot.editMessageMedia(
                 {
                     type: 'photo',
-                      media: 'https://i.postimg.cc/8C1dN1ND/In-Shot-20260601-132018798.jpg',
+                      media: 'https://i.postimg.cc/kGVFzf9m/In-Shot-20260601-131940470.jpg',
                     caption: `𝗛𝗢𝗟𝗜 💕🔥
 TODOS MIS MÉTODOS DE PAGO 🥰
 
